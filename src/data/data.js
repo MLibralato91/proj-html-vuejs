f07ed2a7a95259a689d5963f28084959b4a40d20
MLibralato91 <michelelibralato911@gmail.com>
2023-04-26 16:00:58 +0200

    add data.js

---
+++ src/data/data.js
@@ -0,0 +1,157 @@
+const navLinks = [
+  {
+    text: "Home",
+    url: "#",
+    current: true,
+  },
+  {
+    text: "About",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Services",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Blog",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Contact",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Portfolio",
+    url: "#",
+    current: false,
+  }
+];
+
+const services = [
+  {
+    src: '../../public/images/Group-247.png',
+    title: "Data Analysis",
+    text: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+  },
+  {
+    src: "../../public/images/Group-567.png",
+    title: "SEO Optimization",
+    text: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+  },
+  {
+    src: "../../public/images/Group-538.png",
+    title: "Security Data",
+    text: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+  },
+  {
+    src: "../../public/images/Group-566.png",
+    title: "Branding Strategy",
+    text: 'When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper',
+  }
+];
+
+const works = [
+  {
+    src: "../../public/images/8wa60okr-1-790x576.jpg",
+    title: 'Basket of Flower on table'
+  },
+  {
+    src: "../../public/images/DRY-1-790x576.jpg",
+    title: 'Purinky Products'
+  },
+  {
+    src: "../../public/images/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg",
+    title: 'Satisfy Poster'
+  },
+  {
+    src: "../../public/images/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg",
+    title: 'Mock-up Template'
+  },
+];
+
+const people = [
+  {
+    src: "../../public/images/szabo-viktor-1266895-unsplash-1024x1024.png",
+    name: 'Michkel Anegl',
+    role: 'Marketing Management'
+  },
+  {
+    src: "../../public/images/philipe-cavalcante-539505-unsplash-1024x1024.png",
+    name: 'Michkel Anegl',
+    role: 'Marketing Management'
+  },
+  {
+    src: "../../public/images/fabio-spinelli-695744-unsplash-1024x1024.png",
+    name: 'Michkel Anegl',
+    role: 'Marketing Management'
+  },
+];
+
+const footerLinks = [
+  {
+    title: 'Quick Links',
+    links: [
+      {
+        text: "Services",
+        url: "#",
+      },
+      {
+        text: "Contact",
+        url: "#",
+      },
+      {
+        text: "Blog",
+        url: "#",
+      }
+    ]
+  },
+  {
+    title: 'Resoirces',
+    links: [
+      {
+        text: "Art Design",
+        url: "#",
+      },
+      {
+        text: "Computer",
+        url: "#",
+      },
+      {
+        text: "IT News",
+        url: "#",
+      },
+      {
+        text: "Networking",
+        url: "#",
+      },
+      {
+        text: "Web Security",
+        url: "#",
+      }
+    ]
+  },
+  {
+    title: 'Contact US',
+    links: [
+      {
+        text: "Address:",
+        info: "Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051",
+        url: "#",
+      },
+      {
+        text: "Phone:",
+        info: "+99 (0) 101 0000 888",
+        url: "#",
+      },
+      {
+        text: "Email:",
+        info: "Info@yourdomain.com",
+        url: "#",
+      },
+    ]
+  }
+];
